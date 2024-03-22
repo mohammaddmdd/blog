dcdd7d8de9201dd87d95f2917cebec5c8d3c5705
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2024-03-22 23:08:33 +0330

    Add simple Celery task for adding numbers

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-
+from django.http import JsonResponse
+from .tasks import add
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -11,3 +12,14 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+def add_numbers(request):
+    # Assuming you pass 'x' and 'y' as query parameters to the view
+    x = int(request.GET.get('x', 0))
+    y = int(request.GET.get('y', 0))
+
+    # Calling the asynchronous task
+    result = add.delay(x, y)
+
+    return JsonResponse({'status': 'The add task has been called. Check Celery for results.'})
