d73f43a2cd2ff85fc9fd1286f28a5a2c69b269c9
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2024-03-24 01:37:15 +0330

    Add Django Ninja to requirements.txt and INSTALLED_APPS

--- blog/settings.py
+++ blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog_app',
-    'rest_framework'
+    'rest_framework',
+    'ninja'
 ]
 
 MIDDLEWARE = [
