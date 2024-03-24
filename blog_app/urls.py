6cf57fe21686fef7c36b8929eb3ba5e0ce85090e
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2024-03-24 04:46:40 +0330

    Add Token Authentication Endpoint

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import register
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -8,4 +10,6 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', register, name='register'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
