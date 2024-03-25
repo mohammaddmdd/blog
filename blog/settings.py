2c75c3950ac7c7ebcee59911018720c2b8015238
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2024-03-25 07:05:59 +0330

    Dynamically Configure Redis in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
