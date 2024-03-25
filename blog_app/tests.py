1671f3bdc432039685420103deff07f02e894bcd
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2024-03-26 02:56:09 +0330

    Add Serializer Tests

--- blog_app/tests.py
+++ blog_app/tests.py
@@ -1,11 +1,13 @@
 # blog/tests.py
 from django.test import TestCase
-from .models import Post
+from .models import Post, Comment
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+
+from .serializers import PostSerializer, CommentSerializer, UserSerializer
 
 
 class PostModelTest(TestCase):
@@ -52,3 +54,63 @@ class PostCreateTest(APITestCase):
         data = {'title': 'Title', 'content': 'Content'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PostAccessTest(APITestCase):
+    def test_unauthorized_access(self):
+        response = self.client.get(reverse('post-list'))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PostSerializerTest(TestCase):
+    def setUp(self):
+        self.post_attributes = {
+            'title': 'Test Title',
+            'content': 'Test Content',
+        }
+        self.post = Post.objects.create(**self.post_attributes)
+        self.serializer = PostSerializer(instance=self.post)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), set(['id', 'title', 'content', 'created_at', 'updated_at']))
+
+    def test_title_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data['title'], self.post_attributes['title'])
+
+
+class CommentSerializerTest(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(title='Test Post', content='Post Content')
+        self.comment_attributes = {
+            'post': self.post,
+            'text': 'Main comment',
+            'email': 'test@example.com',
+        }
+        self.comment = Comment.objects.create(**self.comment_attributes)
+        self.reply = Comment.objects.create(**self.comment_attributes, parent=self.comment)
+        self.serializer = CommentSerializer(instance=self.comment)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), set(['id', 'post', 'parent', 'text', 'email', 'created_at' ]))
+
+
+
+class UserSerializerTest(TestCase):
+    def setUp(self):
+        self.user_data = {
+            'username': 'testuser',
+            'password': 'testpassword123',
+            'email': 'testuser@example.com',
+        }
+        self.serializer = UserSerializer(data=self.user_data)
+
+    def test_serializer_valid(self):
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_user_creation(self):
+        self.serializer.is_valid()
+        user = self.serializer.save()
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+        self.assertTrue(user.check_password(self.user_data['password']))
