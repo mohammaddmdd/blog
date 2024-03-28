acb21ed533c012f28a40202802b945690d52f501
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2024-03-29 01:24:54 +0330

    Integrate Django Ninja API with project URLs

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from rest_framework.authtoken.views import obtain_auth_token
 from .views import register
+from blog_app.api import api as ninja_api
+
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -12,4 +14,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', register, name='register'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/v2/', ninja_api.urls),
 ]
